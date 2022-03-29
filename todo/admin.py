6869a7ef6e7532b15d841c82f663d67285cc45b7
Iris-Smok <irissmok11@gmail.com>
2022-03-29 14:48:33 +0000

    Rendering data

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,8 @@
+"""
+admin.py
+"""
 from django.contrib import admin
-from .models import Item 
+from .models import Item
 
 # Register your models here.
 admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,20 @@
+"""
+models
+"""
 from django.db import models
 
-# Create your models here.
+
+# Create your models here
+
 class Item(models.Model):
+    """
+    Item table
+    """
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
+        """
+        return Item class name
+        """
         return self.name

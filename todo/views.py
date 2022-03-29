831fc253e9d180da525e0837281371fa81d83fe1
Iris-Smok <irissmok11@gmail.com>
2022-03-29 15:30:44 +0000

    Create add_item.html with bacis form, create a view and new path in urls

--- todo/views.py
+++ todo/views.py
@@ -8,10 +8,17 @@ from .models import Item
 # Create your views here.
 def get_todo_list(request):
     """
-    todo function
+    todo_list.html
     """
     items = Item.objects.all()
     context = {
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    """
+    add_item.html
+    """
+    return render(request, 'todo/add_item.html')
